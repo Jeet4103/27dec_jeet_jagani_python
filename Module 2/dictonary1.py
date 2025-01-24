2a35252caeb8263e3a2694b46024943ff5c688c8
Jeet4103 <jeetpatel4103.com>
2025-01-24 17:07:03 +0530

    refactor input handling to allow multiple values for keys in dictionary

--- Module 2/dictonary1.py
+++ Module 2/dictonary1.py
@@ -1,5 +1,10 @@
 dic = {}
 
-n = int(input("Enter the number of pairs:"))
+n = int(input("Enter the number of keys you want: "))
 
-for i 
+for i in range(n):
+    key = input(f"Enter key {i+1}: ")
+    values = input(f"Enter values for key {i+1}: ").split(',')
+    dic[key] = values
+
+print(dic)
